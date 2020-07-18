2479d5c13feac90713a075ecfd713ba06374c4c6
rhw <ruohanw89@gmail.com>
2020-07-18 17:00:58 -0600

    update prompt question

--- app.js
+++ app.js
@@ -12,12 +12,40 @@
 
 // printProfileData(profileDataArgs);
 // ----------------------- code reference from 9.1 ends---------------------------//
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
+//...
 
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
-  console.log("Portfolio complete! Check out index.html to the output!");
-});
+// const pageHTML = feneratePage(portfolioData);
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require("inquirer");
+
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile("index.html", generatePage(name, github), (err) => {
+//   if (err) throw err;
+//   console.log("Portfolio complete! Check out index.html to the output!");
+// });
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+
+promptUser().then((answers) => console.log(answers));
